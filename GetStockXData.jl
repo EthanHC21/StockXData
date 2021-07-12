4d94bfcd5c444ccdadb861de673d3dd447bdc26f
EthanHC21 <ethan.catalanello@rutgers.edu>
2021-07-12 08:17:45 -0400

    Added specifiable date formatting

--- GetStockXData.jl
+++ GetStockXData.jl
@@ -5,7 +5,7 @@ include("RemUndef.jl");
 import Dates;
 
 # get the frequency
-print("Enter desired frequency (number of data points): ")
+println("Enter desired frequency (number of data points): ")
 frequency = parse(Int64, readline());
 
 # get URL
@@ -33,6 +33,26 @@ if (!isdir("out"))
     mkdir("out")
 end
 
+# get optimization type from user
+println("Format date values for spreadsheets (S), Raw Unix (U), or None (N)?");
+optimizeVal = readline();
+# check if this input was valid. 
+if (lowercase(optimizeVal) == "s" || lowercase(optimizeVal) == "u" || lowercase(optimizeVal) == "n")
+    validInput = true;
+else
+    validInput = false;
+end
+# keep asking them until it's valid
+while(!validInput)
+    println("Invalid input. Please enter \"S\" for spreadsheets, \"U\" for Unix, or \"N\" for no optimization.");
+    global optimizeVal = readline();
+
+    # check again
+    if (lowercase(optimizeVal) == "s" || lowercase(optimizeVal) == "u" || lowercase(optimizeVal) == "n")
+        global validInput = true;
+    end
+end
+
 # print to text file
 for i = 1:length(sizes)
 
@@ -42,7 +62,21 @@ for i = 1:length(sizes)
 
     # print the data
     for j = 1:frequency
-        println(f, string(Dates.datetime2unix(priceData[i][1][j]), ",", priceData[i][2][j]));
+        
+        if (lowercase(optimizeVal) == "s") # spreadsheet optimization (1970-01-01 = 25569)
+
+            println(f, string((Dates.datetime2unix(priceData[i][1][j])/86400 + 25569), ",", priceData[i][2][j]));
+
+        elseif (lowercase(optimizeVal) == "u") # Unix optimization
+
+            println(f, string(Dates.datetime2unix(priceData[i][1][j]), ",", priceData[i][2][j]));
+
+        else # no optimization
+
+            println(f, string(priceData[i][1][j], ",", priceData[i][2][j]));
+
+        end
+        
     end
 
 end
